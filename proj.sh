7606b332c2081e3b528a62fb5846838c6ff32611
Nanjiang Shu <nanjiang.shu@gmail.com>
2024-06-29 17:26:12 +0200

    remove potential newline in certbot_opts

--- proj.sh
+++ proj.sh
@@ -15,7 +15,7 @@ deploy-with-ssl() {
     sleep 5s
     if [ "$hostnames" != "$hostname_with_ssl" ]; then # need to create the certificate first
         docker compose -f docker-compose.yml --env-file .env-no-ssl up -d
-        certbot_opts=$(echo "$hostnames" | sed 's/ /\ -d\ /g; s/^/-d\ /')
+        certbot_opts=$(echo "$hostnames" | sed 's/ /\ -d\ /g; s/^/-d\ /' | tr '\n' ' ' )
         echo "docker compose exec certbot certbot certonly --webroot -w /var/www/letsencrypt --agree-tos --no-eff-email --email nanjiang@bioshu.se $certbot_opts"
         docker compose exec certbot certbot certonly --webroot -w /var/www/letsencrypt --agree-tos --no-eff-email --email nanjiang@bioshu.se $certbot_opts
         docker compose stop nginx  
